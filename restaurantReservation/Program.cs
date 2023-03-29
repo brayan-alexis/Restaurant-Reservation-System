d8d981ebc1639e79fae607a73f4415ba26b6b284
brayan-alexis <brayanzxd@gmail.com>
2023-03-29 15:26:24 -0700

    v0.1

--- restaurantReservation/Program.cs
+++ restaurantReservation/Program.cs
@@ -9,3 +9,17 @@ Console.WriteLine("Hello, welcome to Alx Restaurant!");
 
 string[] userNames = new string[10] { "Alx", "", "", "", "", "", "", "", "", "" };
 int arrayCurrentIndex = 1;
+bool userType;
+
+while (true)
+{
+    Console.WriteLine("Are you a registered user? Write true, or false to register.");
+    userType = Convert.ToBoolean(Console.ReadLine());
+
+    if (userType == true)
+    {
+        Console.WriteLine("Hello again, you are a registered user, please enter your user name");
+        string usertToSearch = Console.ReadLine();
+        Console.WriteLine("The user you searched is {0}", usertToSearch);
+    }
+}
