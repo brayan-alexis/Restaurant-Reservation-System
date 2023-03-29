5ce91b5c262b4704e07bc0d28c4044a51f1ef949
brayan-alexis <brayanzxd@gmail.com>
2023-03-28 23:16:20 -0700

    Add project files.

---
+++ restaurantReservation/Program.cs
@@ -0,0 +1,11 @@
+﻿/* 
+ * ¡INSTRUCTIONS!
+ * 1.- Validar si el Usuario existe o debe registrarse y generar un sistema de Login.
+ * 2.- El programa debe dar la bienvenida a cada usuario existente 
+ * 3.- El programa se repetirá hasta registrar 10 personas
+*/ 
+
+Console.WriteLine("Hello, welcome to Alx Restaurant!");
+
+string[] userNames = new string[10] { "Alx", "", "", "", "", "", "", "", "", "" };
+int arrayCurrentIndex = 1;
