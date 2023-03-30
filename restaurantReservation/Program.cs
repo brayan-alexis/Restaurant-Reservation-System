ea6bb98d61a6fca6bd26ff697b43b6886a960e07
brayan-alexis <brayanzxd@gmail.com>
2023-03-29 17:47:29 -0700

    v 0.2

--- restaurantReservation/Program.cs
+++ restaurantReservation/Program.cs
@@ -13,13 +13,36 @@ bool userType;
 
 while (true)
 {
-    Console.WriteLine("Are you a registered user? Write true, or false to register.");
+    // Verifica si hay espacio en el resturante
+    if(arrayCurrentIndex == userNames.Length)
+    {
+        Console.WriteLine("    The restaurant is full, try again later...");
+        Environment.Exit(0);
+    }
+
+    Console.WriteLine("\n   Are you a registered user? Write true, or false to register.");
     userType = Convert.ToBoolean(Console.ReadLine());
 
     if (userType == true)
     {
-        Console.WriteLine("Hello again, you are a registered user, please enter your user name");
+        Console.WriteLine("Hello again, you are a registered User, please enter your User Name");
         string usertToSearch = Console.ReadLine();
         Console.WriteLine("The user you searched is {0}", usertToSearch);
+        int index = Array.IndexOf(userNames, usertToSearch);
+        if (index == -1)
+        {
+            Console.WriteLine("User not found, try again or register");
+        }
+        else
+        {
+            Console.WriteLine("Welcome {0}, it's a preasure to give you food ", userNames[index]);
+        }
+    }
+    else if (userType == false) {
+        Console.WriteLine("Please write and remember your User Name");
+        userNames[arrayCurrentIndex] = Console.ReadLine();
+        Console.WriteLine("Your User has been save successfully \n" +
+            "Your User Name is: {0}", userNames[arrayCurrentIndex]);
+        arrayCurrentIndex++;
     }
 }
