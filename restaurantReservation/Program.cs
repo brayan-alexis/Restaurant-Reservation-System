f21673e602f560beca3d1eec55d083962db4d791
brayan-alexis <brayanzxd@gmail.com>
2023-03-29 19:23:39 -0700

    v1.0

--- restaurantReservation/Program.cs
+++ restaurantReservation/Program.cs
@@ -7,18 +7,21 @@
 
 Console.WriteLine("Hello, welcome to Alx Restaurant!");
 
-string[] userNames = new string[10] { "Alx", "", "", "", "", "", "", "", "", "" };
-int arrayCurrentIndex = 1;
+string[] userNames = new string[10] { "Brayan", "Alexis", "Alx", "Juan", "Luis", "Mario", "Laura", "Joel", "Cass", "" };
+int arrayCurrentIndex = 9;
 bool userType;
 
-while (true)
+// Verifica si hay espacio en el resturante
+while (arrayCurrentIndex < userNames.Length)
 {
+    /*
     // Verifica si hay espacio en el resturante
     if(arrayCurrentIndex == userNames.Length)
     {
         Console.WriteLine("    The restaurant is full, try again later...");
         Environment.Exit(0);
     }
+    */
 
     Console.WriteLine("\n   Are you a registered user? Write true, or false to register.");
     userType = Convert.ToBoolean(Console.ReadLine());
@@ -46,3 +49,15 @@ while (true)
         arrayCurrentIndex++;
     }
 }
+
+// Resultados una vez que el restaurante tenga 10 Users registrados
+Console.WriteLine("    The restaurant is full, try again later...\n These are the guest for the dinner");
+int auxIndex = 0;
+foreach (string name in userNames)
+{
+    Console.WriteLine($"User Number: {auxIndex+1} and User Name: {name}");
+    //Console.WriteLine("User Number: {0} and User Name: {1}", auxIndex+1, name);                  Alternativa 1
+    //Console.WriteLine("User Number: {0} and User Name: {1}", auxIndex+1, userNames[auxIndex]);   Alternativa 2
+    auxIndex++;
+}
+Environment.Exit(0);
